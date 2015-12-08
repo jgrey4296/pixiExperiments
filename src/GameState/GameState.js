871c4001c626a0702e450279d221e37d121067ab
John Grey <jgrey@ucsc.edu>
2015-12-08 00:53:49 -0800

    added in simple collisions and display groups

--- src/GameState/GameState.js
+++ src/GameState/GameState.js
@@ -2,6 +2,8 @@ define(['json!data/scene1.json','underscore','phaser'],function(scene,_){
 
     //The game state to describe a room you are in
     var GameState = function(game){
+        this.physicsType = Phaser.Physics.ARCADE;
+        
         this.game = game;
         //Current State info:
         this.currentRoom = null;
@@ -12,9 +14,7 @@ define(['json!data/scene1.json','underscore','phaser'],function(scene,_){
         this.actors = {};
 
         //groups
-        this.roomItems;
-        this.roomActors;
-        this.roomDoors;
+        this.groups = {};
         
         //Reasoning / AI
         this.rete = null;
@@ -45,17 +45,7 @@ define(['json!data/scene1.json','underscore','phaser'],function(scene,_){
         //create the starting room:
         this.buildRoom(scene[0]);
 
-        //create the character:
-        this.controllableActor = this.add.sprite(50,this.game.height-150,'pig');
-        this.game.physics.enable(this.controllableActor,Phaser.Physics.ARCADE);
-        this.controllableActor.anchor.setTo(.5,1);
-        this.controllableActor.height = 50;
-        this.controllableActor.facing = 'right';
-        this.controllableActor.body.collideWorldBounds = true;
-        this.controllableActor.body.fixedRotation = true;
-        this.controllableActor.body.bounce.y = 0.2;
-
-
+        this.physics.arcade.setBounds(0,0,this.game.width,this.game.height-75);
         
     };
 
@@ -84,6 +74,12 @@ define(['json!data/scene1.json','underscore','phaser'],function(scene,_){
             }
         }
 
+        this.game.physics.arcade.collide(this.groups.actors,this.groups.actors,
+                                      function(s1,s2){
+                                          console.log("Collided: ",s1.name, s2.name);
+                                      },null,this);
+
+        this.game.physics.arcade.collide(this.groups.actors,this.groups.objects);
         
     };
 
@@ -93,20 +89,88 @@ define(['json!data/scene1.json','underscore','phaser'],function(scene,_){
         this.background.width = this.game.width;
         this.background.height = this.game.height;
         
-        //build the floor, ceiling, and walls
-        var sprite;
-        room.items.forEach(function(d){
-            sprite = this.add.sprite(d.position.x,d.position.y,d.assetName,d.frame);
-            sprite.anchor.setTo(0.5,1);
-            if(d.physical){
-                this.game.physics.enable(sprite);
-                sprite.body.collideWorldBounds = true;
-                
-            }
+        //todo:build the floor, ceiling, and walls
+        //TODO: sort items by y value
+        room.items.sort(function(a,b){
+            return a.position.y - b.position.y;
+        });
+        
+        var pre = room.items.filter(function(d){
+            return d.position.y < this.game.height - 75;
+        },this);
+
+        var post = room.items.filter(function(d){
+            return d.position.y >= this.game.height - 75;
+        },this);
+
+        console.log("Filtered:",pre,post);
+        
+        pre.forEach(function(d){
+            this.buildItem(d);
+        },this);
+
+        room.actors.forEach(function(d){
+            this.buildActor(d);
             
         },this);
-        
+
+        post.forEach(function(d){
+            this.buildItem(d);
+        },this);
     };
+
+    
+    GameState.prototype.buildActor = function(d){
+        if(d.group === undefined) d.group = "default";
+        if(d.physical === undefined) d.physical = false;
+        //create the group if necessary
+        if(this.groups[d.group] === undefined) this.groups[d.group] = this.add.group(undefined,d.group,false,d.physical,this.physicsType);
+
+        var actor = this.groups[d.group].create(d.position.x,d.position.y,d.assetName,d.frame);
+        actor.anchor.setTo(.5,1);
+
+        //Custom properties:
+        actor.name = d.name;
+        actor.facing = d.facing;
+
+        if(actor.facing === 'left'){
+            actor.scale.x = -1;
+        }
+        
+        if(d.physical){
+            this.game.physics.enable(actor,this.physicsType);
+            actor.body.collideWorldBounds = true;
+            actor.body.fixedRotation = true;
+            actor.body.bounce.y = 0.2;
+            
+        }
+        if(d.controllable){
+            this.controllableActor = actor;
+        }
+    };
+
+    GameState.prototype.buildItem = function(d){
+            //if there is no group defined:
+            if(d.group === undefined) d.group = "default";
+            if(d.physical === undefined) d.physical = false;
+            //create the group if necessary
+            var parent = undefined;
+            if(d.invisible) parent = null;
+                        
+            if(this.groups[d.group] === undefined) this.groups[d.group] = this.add.group(parent,d.group,false,d.physical,this.physicsType);
+            
+            var sprite = this.groups[d.group].create(d.position.x,d.position.y,d.assetName,d.frame);
+            sprite.anchor.setTo(0.5,1);
+            if(d.physical){
+                //this.game.physics.enable(sprite);
+                sprite.body.collideWorldBounds = true;
+                if(d.immovable){
+                    sprite.body.immovable = d.immovable;
+                }
+            }
+    };
+    
+    
 
     return GameState;
 });
