ae03aca940209de0b15c005531c934fc39becac2
John Grey <jgrey@ucsc.edu>
2016-03-27 07:28:39 -0700

    simplified room, added in camera movement

--- src/Extensions/Room.js
+++ src/Extensions/Room.js
@@ -8,65 +8,31 @@ define(['underscore','./Actor','./Item','./Door','phaser'],function(_,Actor,Item
        @alias Room
        @augments Phaser.Group
      */
-    var Room = function(game,description){
+    var Room = function(game,description,position,size){
+        if(position === undefined) { position = [0,0]; }
+        if(size === undefined) { size = [100,100]; }
         Phaser.Group.call(this,game,null,description.name);
+
+        this.x = position[0];
+        this.y = position[1];
+        
         /** id */
         this.id = description.id;
         
         /** Background */
         var background = new Phaser.Sprite(this.game,0,0,description.background,description.backgroundFrame);
-        background.width = this.game.width;
-        background.height = this.game.height;
+        background.width = size[0];
+        background.height = size[1];
         if(description.tint !== undefined){
             background.tint = parseInt(description.tint,16);
-            console.log("Set background tint to:",background.tint);
         }
         this.add(background);
 
-        /** Connections to other rooms */
-        this.connections = description.connections;
         /** Actors in the room */
         this.actors = {};
         /** Draw groups in the room */
         this.groups = {};
 
-        //Build the room:
-        //walls
-        description.walls.forEach(d=>this.buildWall(d));
-        // //doors
-        description.doors.forEach(d=>this.buildDoor(d));
-        //items
-        description.items.forEach(d=>this.buildItem(d));
-        //actors
-        description.actors.forEach(d=>this.buildActor(d));
-
-        
-        // //todo:build the floor, ceiling, and walls
-        // //TODO: sort items by y value
-        // room.items.sort(function(a,b){
-        //     return a.position.y - b.position.y;
-        // });
-        
-        // var pre = room.items.filter(function(d){
-        //     return d.position.y < this.game.height - 75;
-        // },this);
-
-        // var post = room.items.filter(function(d){
-        //     return d.position.y >= this.game.height - 75;
-        // },this);
-
-        // console.log("Filtered:",pre,post);
-        
-        // pre.forEach(function(d){
-        //     this.buildItem(d);
-        // },this);
-
-        // post.forEach(function(d){
-        //     this.buildItem(d);
-        // },this);
-
-
-        
     };
     
     Room.prototype = Object.create(Phaser.Group.prototype);
@@ -95,130 +61,35 @@ define(['underscore','./Actor','./Item','./Door','phaser'],function(_,Actor,Item
         tileSprite.body.allowGravity = false;
     };
 
-    /** Builds a {@link Door} in the room
-        @method
-     */
-    Room.prototype.buildDoor = function(d){
-        
-        console.log("Building door:",d);
-        d.group = "doors";
-
-        var parent = undefined;
-        if(d.invisible) parent = null;
-
-        if(this.groups[d.group] === undefined){
-            var newGroup = new Phaser.Group(this.game,this,d.group,false,d.physical,this.physicsType);
-            this.groups[d.group] = newGroup;
-        }
-
-        var door = new Door(this.game,d.position.x,d.position.y,d.assetName,d.name,d.connectsTo);
-        door.height = d.size.y;
-        door.width = d.size.x;
-        
-        this.groups[d.group].add(door);
-        door.anchor.setTo(0.5,1);
-
-        this.game.physics.enable(door);
-        door.body.collideWorldBounds = true;
-        door.body.immovable = true;
-        door.body.allowGravity = false;
-        
-        console.groupEnd();
-    };
-
-    /** Builds an {@link Item} in the room
-        @method
-     */
-    Room.prototype.buildItem = function(d){
-        console.log("building Item:",d);
-        //if there is no group defined:
-        if(d.group === undefined) d.group = "default";
-        if(d.physical === undefined) d.physical = false;
-        //create the group if necessary
-        var parent = undefined;
-        if(d.invisible) parent = null;
-        
-        if(this.groups[d.group] === undefined){
-            console.log("creating group: ",d.group);
-            var newGroup = new Phaser.Group(this.game,this,d.group,false,d.physical,this.physicsType);
-            this.groups[d.group] = newGroup;
-        }
-        
-        var item = new Item(this.game,d.position.x,d.position.y,d.assetName,d.frame,d.name);
-        this.groups[d.group].add(item);
-        item.anchor.setTo(0.5,1);
-        
-        if(d.physical){
-            this.game.physics.enable(item);
-            item.body.collideWorldBounds = true;
-            if(d.immovable){
-                item.body.immovable = d.immovable;
-                item.body.allowGravity = false;
-            }
-        }
-
-        console.groupEnd();
-    };
-
-    /** Builds an {@link Actor} in the room
-        @method
-     */
-    Room.prototype.buildActor = function(d){
-        //TODO: customise the group.classType for custom classes
-        var actor = new Actor(this.game,d.position.x,d.position.y,d.assetName,d.frame,d.name,d.facing,d.controllable,d.width,d.height);
-
-        if(d.dimensions){
-            actor.height = d.dimensions.height;
-            actor.width = d.dimensions.width;
-        }
-        
-        if(d.physical){
-            this.game.physics.enable(actor);
-            actor.body.collideWorldBounds = true;
-            actor.body.fixedRotation = true;
-            actor.body.bounce.y = 0.2;
-        }
-
-        //if the actor is defined to have any animations:
-        _.keys(d.animations).forEach(function(e){
-            actor.registeredAnimations[e] = d.animations[e];
-        });
-
-        this.addActor(actor);
-
-        console.log(this.groups);
-        console.groupEnd();
-    };
-
     /** Updates the room, running collisions within the room
         @method
     */
     Room.prototype.update = function(){
         var roomRef = this;
         //check for collisions
-        if(this.groups.actors !== undefined){
-            this.game.physics.arcade.collide(this.groups.actors,this.groups.actors);
-            this.game.physics.arcade.collide(this.groups.actors,this.groups.objects);
-            this.game.physics.arcade.collide(this.groups.actors,this.groups.walls);
-            this.game.physics.arcade.collide(this.groups.actors,this.groups.doors,function(actor,door){
-                if(!actor.controllable){
-                    //move the actor through the door
-                    roomRef.game.world.remove(actor);
-                    delete roomRef.actors[actor.name];
-                    var room = roomRef.getRoomFromDoor(door);
-                    room.addActor(actor);
-                }else{
-                    //get the room using the door's connection
-                    var room = roomRef.getRoomFromDoor(door);
-                    roomRef.game.state.getCurrentState().changeRoom(room);
-                }
-            });
-        }
-        //Collide objects with the walls
-        this.game.physics.arcade.collide(this.groups.walls,this.groups.objects);
+        // if(this.groups.actors !== undefined){
+        //     this.game.physics.arcade.collide(this.groups.actors,this.groups.actors);
+        //     this.game.physics.arcade.collide(this.groups.actors,this.groups.objects);
+        //     this.game.physics.arcade.collide(this.groups.actors,this.groups.walls);
+        //     this.game.physics.arcade.collide(this.groups.actors,this.groups.doors,function(actor,door){
+        //         if(!actor.controllable){
+        //             //move the actor through the door
+        //             roomRef.game.world.remove(actor);
+        //             delete roomRef.actors[actor.name];
+        //             var room = roomRef.getRoomFromDoor(door);
+        //             room.addActor(actor);
+        //         }else{
+        //             //get the room using the door's connection
+        //             var room = roomRef.getRoomFromDoor(door);
+        //             roomRef.game.state.getCurrentState().changeRoom(room);
+        //         }
+        //     });
+        // }
+        // //Collide objects with the walls
+        // this.game.physics.arcade.collide(this.groups.walls,this.groups.objects);
         
-        //run AI for each actor
-        _.values(this.actors).forEach(d=>d.update());
+        // //run AI for each actor
+        // _.values(this.actors).forEach(d=>d.update());
         
         //perform movements
 
@@ -229,40 +100,6 @@ define(['underscore','./Actor','./Item','./Door','phaser'],function(_,Actor,Item
     };
 
 
-    /** Gets the room object the door specified connects to
-        @method
-        @param door
-     */
-    Room.prototype.getRoomFromDoor = function(door){
-        var roomId = this.connections[door.connection];
-        return this.game.state.getCurrentState().rooms[roomId];
-    };
-
-    /** Adds an existing actor to the room
-        @method
-        @param actor
-        @param group
-        @param physical
-        @param physicsType
-    */
-    Room.prototype.addActor = function(actor,group,physical,physicsType){
-        if(group === undefined) group = "actors";
-        if(physical === undefined) physical = true;
-        if(physicsType === undefined) physicsType = "arcade";
-
-        //create the group if necessary
-        if(this.groups[group] === undefined){
-            console.log("Creating group:",group);
-            var newGroup = new Phaser.Group(this.game,this,group,false,physical,physicsType);
-            this.groups[group] = newGroup;
-        }
-
-        this.groups[group].add(actor);
-        this.actors[actor.name] = actor;
-        
-    };
-    
-    
     return Room;
 
 });
--- src/GameState/GameState.js
+++ src/GameState/GameState.js
@@ -1,4 +1,7 @@
-define(['json!data/scene1.json','underscore','../Extensions/SpeechBubble','../Extensions/Actor','../Extensions/Room','phaser'],function(scene,_,SpeechBubble,Actor,Room,Phaser){
+/* jshint esversion : 6 */
+define(['json!data/scene1.json','underscore','../Extensions/SpeechBubble','../Extensions/Actor','../Extensions/Room','phaser','text!data/mazePaths','ClingoParser'],function(scene,_,SpeechBubble,Actor,Room,Phaser,mazePaths,ClingoParser){
+    var mazeSize = 8;
+    
     /**
        Main state of a game. In this case emulating BoI, loaded from data/scene1.json
        @constructor
@@ -21,13 +24,18 @@ define(['json!data/scene1.json','underscore','../Extensions/SpeechBubble','../Ex
         this.fps = 30;
         
         /** All Rooms */
-        this.rooms = {};
+        this.maze = Array(mazeSize).fill(0).map(d=>Array(mazeSize).fill(0).map(d=>null));
+        /** Maze paths loaded from clingo output as answer sets of fact tuples */
+        this.mazePaths = ClingoParser(mazePaths,'used');
+        //room size:
+        this.roomSize = [this.game.width,this.game.height];
+
         /** All Actors */
         this.actors = {};
 
         /** The Reasoning System */
         this.rete = null;
-
+        
         if(scene !== undefined){
             this.scene = scene;
         }
@@ -52,28 +60,46 @@ define(['json!data/scene1.json','underscore','../Extensions/SpeechBubble','../Ex
         console.log("GameState create");
         this.game.time.desiredFps = this.fps;
         this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.cameraOffset = [this.game.width/2,this.game.height/2];
+        
         //setup physics:
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.physics.arcade.gravity.y = this.gravityAmnt;
+        
+        //set the boundaries of the game world:
+        //larger than the world itself so the camera can centre on a room
+        //this.game.world.setBounds(0,0,(mazeSize+4) * this.roomSize[0],(mazeSize+4) * this.roomSize[1]);
+        //Set world boundaries
+        this.physics.arcade.setBounds(0,0,(mazeSize+1)*this.roomSize[0],(mazeSize+1)*this.roomSize[1]);
 
-        //Create each room,
-        //and add the origin room
-        scene.forEach(function(d){
-            var newRoom = this.buildRoom(d);
-            //Only add the current room which is the origin, to the world
-            if(d.origin){
-                this.currentRoom = newRoom;
-                this.game.world.add(newRoom);
+        //select an answer set and build room around from it
+        _.sample(this.mazePaths).forEach(function(d){
+            if(this.maze[parseInt(d[0])][parseInt(d[1])] !== null){
+                //room has already been built
+                return;
             }
+            let randomRoomDescription = _.sample(scene),
+                position = this.gridPositionToWorldPosition(parseInt(d[0]), parseInt(d[1])),
+                size = this.roomSize,
+                newRoom = this.buildRoom(randomRoomDescription,position,size);
+            this.maze[parseInt(d[0])][parseInt(d[1])] = newRoom;
         },this);
 
-        //Set the controllable character from the current room:
-        var actors = _.values(this.currentRoom.actors);
-        this.controllableActor = _.find(_.values(actors),d=>d.controllable === true);
-        console.log("set controllable actor to:",this.controllableActor);
+        //Set the camera to the first room:
+        this.game.camera.bounds = null;
+        this.game.camera.setSize(this.roomSize[0],this.roomSize[1]);
+        this.roomMovement = {};
+        this.roomMovement.current = [0,0];
+        this.centreCameraOnCurrentRoom();
+
+        //Register room movement presses
+        this.roomMovement.left = this.game.input.keyboard.addKey(Phaser.Keyboard.J);
+        this.roomMovement.right = this.game.input.keyboard.addKey(Phaser.Keyboard.L);
+        this.roomMovement.up = this.game.input.keyboard.addKey(Phaser.Keyboard.I);
+        this.roomMovement.down = this.game.input.keyboard.addKey(Phaser.Keyboard.K);
+        this.roomMovement.showAll = this.game.input.keyboard.addKey(Phaser.Keyboard.M);
         
-        //Set world boundaries
-        this.physics.arcade.setBounds(0,0,this.game.width,this.game.height);
+        //this.game.input.keyboard.addKeyCapture([Phaser.Keyboard.J,Phaser.Keyboard.L,Phaser.Keyboard.I,Phaser.Keyboard.K]);
     };
 
     /**
@@ -82,9 +108,8 @@ define(['json!data/scene1.json','underscore','../Extensions/SpeechBubble','../Ex
      */
     GameState.prototype.update = function(){
         //Update the room
-        this.currentRoom.update();
-        
-        //Call movement control on the controllable actor:
+
+        //**** ACTOR CONTROL
         if(this.controllableActor){
             //this.controllableActor.move();
             //get the cursor keys, move the playable character as necessary
@@ -102,6 +127,43 @@ define(['json!data/scene1.json','underscore','../Extensions/SpeechBubble','../Ex
                 this.controllableActor.move('right');
             }
         }
+        //**** SHOW ENTIRE MAP
+        if(this.roomMovement.showAll.downDuration(200)){
+            if(this.roomMovement.mapRecent) { return; }
+            if(!this.roomMovement.showMap){
+                //show the whole map
+                this.game.camera.setSize(this.roomSize[0] * mazeSize,this.roomSize[1] * mazeSize);
+                this.game.camera.scale.x = 1/mazeSize;
+                this.game.camera.scale.y = 1/mazeSize;
+                this.game.camera.setPosition(0,0);
+                this.roomMovement.showMap = true;
+            }else{
+                //go back to showing just a room
+                this.game.camera.setSize(this.roomSize[0],this.roomSize[1]);
+                this.game.camera.scale.x = 1;
+                this.game.camera.scale.y = 1;
+                this.roomMovement.showMap = false;
+                this.centreCameraOnCurrentRoom();
+            }
+            this.roomMovement.mapRecent = true;
+        }else{
+            this.roomMovement.mapRecent = false;
+        }
+
+        //**** MOVE MAP
+        if(!this.roomMovement.showMap){
+            if(this.roomMovement.left.downDuration(200)){
+                this.moveRoom(-1,0);
+            }else if(this.roomMovement.right.downDuration(200)){
+                this.moveRoom(1,0);
+            }else if(this.roomMovement.up.downDuration(200)){
+                this.moveRoom(0,-1);
+            }else if(this.roomMovement.down.downDuration(200)){
+                this.moveRoom(0,1);
+            }else{
+                this.roomMovement.movedRecently = false;
+            }
+        }
     };
 
     //------------------------------
@@ -112,32 +174,11 @@ define(['json!data/scene1.json','underscore','../Extensions/SpeechBubble','../Ex
        @method
      */
     
-    GameState.prototype.buildRoom = function(room){
-        console.log("Building Room:",room);
-        var newRoom = new Room(this.game,room);
-        this.rooms[newRoom.id] = newRoom;
+    GameState.prototype.buildRoom = function(room,position,size){
+        //console.log("Building Room:",room);
+        var newRoom = new Room(this.game,room,position,size);
+        this.game.world.add(newRoom);
         return newRoom;
-    };
-
-    /** Changes the current room, moving the controllable actor as well
-        @method
-     */
-    GameState.prototype.changeRoom = function(room){
-        if(this.currentRoom !== null){
-            this.game.world.remove(this.controllableActor);
-            this.game.world.remove(this.currentRoom);
-        }
-
-        //get the room
-        if(room){
-            this.currentRoom = room;
-            this.game.world.add(room);
-            this.currentRoom.addActor(this.controllableActor);
-        }else{
-            console.log(room,this.rooms);
-            throw new Error("room is null");
-        }
-        console.log("Current Room:",this.currentRoom);
     };
 
     /**
@@ -145,8 +186,49 @@ define(['json!data/scene1.json','underscore','../Extensions/SpeechBubble','../Ex
        @method
      */
     GameState.prototype.render = function(){
+        //this.game.debug.cameraInfo(this.game.camera,500,32);
         //this.game.debug.body(this.controllableActor);
 
+    };
+
+    /**
+       Utility method to convert an element of this.maze to world coordinates,
+       ie: getting this.maze[i][j] -> [x,y]
+       @param i
+       @param j
+     */
+    GameState.prototype.gridPositionToWorldPosition = function(i,j){
+        let position = [
+            i * this.roomSize[0],
+            j * this.roomSize[1]
+        ];
+        return position;
+    };
+
+    GameState.prototype.centreOfRoom = function(i,j){
+        let position = this.gridPositionToWorldPosition(i,j);
+        position[0] += this.roomSize[0];
+        position[1] += this.roomSize[1];
+        return position;
+    };
+
+    GameState.prototype.moveRoom = function(i,j){
+        if(this.roomMovement.movedRecently){ return; }
+        let newPos = [this.roomMovement.current[0] + i,
+                      this.roomMovement.current[1] + j];
+        if(0 <= newPos[0]  && newPos[0] < mazeSize
+           && 0 <= newPos[1] && newPos[1] < mazeSize
+          && this.maze[newPos[0]][newPos[1]] !== null){
+            this.roomMovement.current = newPos;
+        }
+        this.roomMovement.movedRecently = true;
+        this.centreCameraOnCurrentRoom();
+    };
+    
+    GameState.prototype.centreCameraOnCurrentRoom = function(){
+        let roomCentre = this.centreOfRoom(this.roomMovement.current[0],this.roomMovement.current[1]),
+            newPosition = [roomCentre[0]-this.cameraOffset[0],roomCentre[1]-this.cameraOffset[1]];
+        this.game.camera.focusOnXY(newPosition[0],newPosition[1]);
     };
     
     return GameState;
