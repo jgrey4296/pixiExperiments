e7f9533142365fdec2fd8ef8c5cb453190d138e3
John Grey <jgrey@ucsc.edu>
2016-06-13 15:09:28 -0700

    debug statements and test

--- src/Extensions/HexBoard.js
+++ src/Extensions/HexBoard.js
@@ -9,19 +9,19 @@ define(['lodash','util','./Hexagon','./HexTexture','../HexLib/HexLib','phaser'],
     */
     let HexBoard = function(game,boardSize,standardRadius,fill,stroke){
         util.debug('start',()=>console.log('Creating Hexboard'));
+        //defaults:
         if(fill === undefined){ fill = 0xFF00FF; }
         if(stroke === undefined){ stroke = 0x00FFFF; }
         if(boardSize === undefined) { boardSize = [25,25]; }
-        if(standardRadius === undefined) { standardRadius = 20; }
+        if(standardRadius === undefined) { standardRadius = 50; }
+        //Setup the group:
         Phaser.Group.call(this,game,null,'HexBoard');
         this.boardSize = boardSize;
         //individual hexagon dimensions
         this.radius = standardRadius;
         this.hexHeight = 2 * this.radius;
-
         //Setup the util internal values
         HexUtil.setBoardSize(this.boardSize[0],this.boardSize[1]);
-        
         //check this size of hex texture exists, if not, create it:
         if(!this.game.cache.checkKey(Phaser.Cache.RENDER_TEXTURE,util.hexTexture(this.radius))){
             util.debug('texture',()=>console.log(`Creating additional texture: ${this.radius}`));
@@ -41,6 +41,10 @@ define(['lodash','util','./Hexagon','./HexTexture','../HexLib/HexLib','phaser'],
             return newHex;
         });
 
+        _.sampleSize(_.shuffle(this.hexagons),200).forEach(d=>d.destroy());//this.removeChild(d));
+
+        console.log(this.hexagons);
+        
         util.debug('hexs',()=>console.log(this.hexagons.map(d=>[d.x,d.y])));
         util.debug('start',()=>console.log('Finished Creating Hexboard'));
     };
