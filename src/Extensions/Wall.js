0cc0231ab9b357ae36919c92c0433a5948bdb9b9
John Grey <jgrey@ucsc.edu>
2016-10-25 05:24:02 -0700

    updating wall constructor

--- src/Extensions/Wall.js
+++ src/Extensions/Wall.js
@@ -1,14 +1,12 @@
 /* jshint esversion : 6 */
 define(['lodash','phaser'],function(_,Phaser){
 
-    var Wall = function(game,x,y,key,frame,tint){
-        Phaser.TileSprite.call(this,game,x,y,100,100,key);
+    var Wall = function(game,x,y,width,height,key,frame,tint,angle){
+        Phaser.TileSprite.call(this,game,x,y,width,height,key);
          if(tint !== undefined){
              this.tint = parseInt(tint,16);
          }
-        this.game.physics.enable(this);
-        this.body.immovable = true;
-        this.body.allowGravity = false;
+        this.angle = angle;
     };
     Wall.prototype = Object.create(Phaser.TileSprite.prototype);
     Wall.prototype.constructor = Wall;
